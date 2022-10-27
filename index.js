3e7a2e5640bd3d7657495c90ce0d5acd91d8ac35
ashimbhowmik <ashimbhowmik239@gmail.com>
2022-10-27 23:06:29 +0600

    [create new api]

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.use(cors());
 
 const courses = require("./Data/courses.json");
 
+app.get("/", (req, res) => {
+  res.send("WellCome To Learning platrom Api Server");
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
 app.get("/courses/:id", (req, res) => {
   const id = req.params.id;
   const course = courses.find((c) => c.id === id);
